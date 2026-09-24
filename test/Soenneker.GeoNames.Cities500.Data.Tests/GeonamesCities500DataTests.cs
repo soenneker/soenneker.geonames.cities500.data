dc1a3b3382ea172e6280808583697ae446f630de
soenneker <jake@soenneker.com>
2026-09-24 09:15:43 -0500

    Refactor file operations to use FileUtil

--- test/Soenneker.GeoNames.Cities500.Data.Tests/GeonamesCities500DataTests.cs
+++ test/Soenneker.GeoNames.Cities500.Data.Tests/GeonamesCities500DataTests.cs
@@ -1,3 +1,6 @@
+using Soenneker.Utils.MemoryStream;
+using Microsoft.Extensions.Logging.Abstractions;
+using Soenneker.Utils.File.Abstract;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -6,12 +9,14 @@ namespace Soenneker.GeoNames.Cities500.Data.Tests;
 
 public sealed class GeonamesCities500DataTests
 {
+    private static readonly IFileUtil _fileUtil = new Soenneker.Utils.File.FileUtil(NullLogger<Soenneker.Utils.File.FileUtil>.Instance, new MemoryStreamUtil());
+
     [Test]
     public async Task Declares_cities500_resource_for_runner_packaging()
     {
         string projectPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "..", "src",
             "Soenneker.GeoNames.Cities500.Data", "Soenneker.GeoNames.Cities500.Data.csproj"));
-        string projectXml = await File.ReadAllTextAsync(projectPath);
+        string projectXml = await _fileUtil.Read(projectPath);
 
         await Assert.That(projectXml).Contains("Resources\\cities500.txt");
     }
